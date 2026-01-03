18cb15cde0ef9b4c0a061c374e0141b98c20e9f9
SkyOfSteel <ivanzots@outlook.com>
2026-01-02 21:51:26 -0500

    Finished the game logic

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 import pygame
-from logger import log_state
+import sys
+from player import Player
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField_object = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        player.cooldown -= dt
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+                    break
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        log_state()
+        # delta time (time between frames) in milliseconds. 0.016 for 60 FPS (1 / 60)
+        dt = game_clock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
